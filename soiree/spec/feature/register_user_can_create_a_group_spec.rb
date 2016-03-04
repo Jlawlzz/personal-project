9f235cac3e5482686bf09e5c91cace322fdd1465
Taylor Moore <Tmoore2272@gmail.com>
2016-03-04 14:27:34 -0700

    Sets up a test for groups

---
+++ soiree/spec/feature/register_user_can_create_a_group_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "User can create a group" do
+  include Capybara::DSL
+  scenario "create a 30 long song playlist" do
+
+    Platform.create(name: 'spotify')
+
+    VCR.use_cassette("returns user") do
+      visit '/'
+      click_on "login with facebook"
+      click_link "Login to Spotify"
+    end
+
+    expect(current_path).to eq '/dashboard'
+    visit '/group_playlist/new'
+
+    expect(current_path).to eq '/group_playlist/new'
+
+    #SELECT GROUP MEMEBERS
+    fill_in "Name", with: "Boss"
+    fill_in "Description", with: "Coolio"
+    expect(page).to have_content "Preferences"
+    click_on "Create Playlist"
+  #
+  #   playlist = Playlist.first
+  #   expect(current_path).to eq playlist_path(playlist.id)
+  #
+  #   expect(20).to eq playlist.songs.count
+  #
+  end
+end
