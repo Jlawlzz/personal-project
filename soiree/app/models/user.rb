f638f78af80e0a05356e117fc618e96ec44532ad
Jordan Lawler <jordan.lawler@colorado.edu>
2016-03-06 18:49:42 -0700

    Added int. test for and refactored group playlists
    Added integration test for group playlists.  Still need to do sad path tests.
    Refactored where particular methods live in playlist.rb to spotify_service.
    Refactored spotify login to spotify_service.

--- soiree/app/models/user.rb
+++ soiree/app/models/user.rb
@@ -5,17 +5,12 @@ class User < ActiveRecord::Base
   has_many :group_users
   has_many :groups, through: :group_users
 
-  def self.omniauth(auth)
-    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
+  def self.facebook_login(auth)
+    FacebookService.login(auth)
+  end
 
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user.name = auth.info.name
-    user.image = auth.info.image
-    user.token = auth.credentials.token
-    user.expires_at = Time.at(auth.credentials.expires_at)
-    user.save!
-    user
+  def self.spotify_login(auth, current_user)
+    SpotifyService.login(auth, current_user)
   end
 
   def user_playlists
@@ -26,15 +21,4 @@ class User < ActiveRecord::Base
     self.playlists.where("preferences @> 'type=>group'")
   end
 
-  def self.spotify_login(auth, current_user)
-    spotify = Platform.find_by(name: 'spotify')
-
-    if !current_user.platforms.where(name: 'spotify').empty?
-      token = current_user.tokens.where(platform: {name: 'spotify'})
-    else
-      token = Token.create(auth: auth.to_json, platform_id: spotify.id)
-      current_user.tokens << token
-    end
-    token
-  end
 end
--- soiree/app/services/spotify_service.rb
+++ soiree/app/services/spotify_service.rb
@@ -1,10 +1,46 @@
 class SpotifyService
 
-  def self.fetch_songs(sanitized_songs)
+  def initialize
+    RSpotify::authenticate(ENV['SPOTIFY_KEY'], ENV['SPOTIFY_SECRET'])
+  end
+
+  def self.login(auth, current_user)
+    spotify = Platform.find_by(name: 'spotify')
+    if !current_user.platforms.where(name: 'spotify').empty?
+      token = current_user.tokens.where(platform: {name: 'spotify'})
+    else
+      token = Token.create(auth: auth.to_json, platform_id: spotify.id)
+      current_user.tokens << token
+    end
+    token
+  end
+
+  def return_playlist(preferences)
+    playlist_type(preferences)
+  end
+
+  def playlist_type(preferences)
+    case preferences['type']
+    when "personal" then Personal::SpotifyLogic.find_songs(preferences)
+    when "group" then Group::SpotifyLogic.find_songs(preferences)
+    end
+  end
+
+  def save_playlist(sanitized_songs, user_auth, playlist_name)
+    tracks = fetch_songs(sanitized_songs)
+    playlist = create_playlist(user_auth, playlist_name)
+    playlist.add_tracks!(tracks)
+  end
+
+  def fetch_songs(sanitized_songs)
     RSpotify::Track.find(sanitized_songs.first(30))
   end
 
-  def self.refresh_playlist(playlist, tracks)
+  def create_playlist(user_auth, playlist_name)
+    user_auth.create_playlist!(playlist_name + ' - Soriee')
+  end
+
+  def refresh_playlist(playlist, tracks)
     playlist.add_tracks!(tracks)
   end
 
