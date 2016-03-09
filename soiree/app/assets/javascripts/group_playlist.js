47a059df2b805d5cefe9caaba05e02632b07675e
Jordan Lawler <jordan.lawler@colorado.edu>
2016-03-09 13:28:00 -0700

    fixed spinner

--- soiree/app/assets/javascripts/group_playlist.js
+++ soiree/app/assets/javascripts/group_playlist.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
 
 function renderGroupSongs(){
 
+  var groupTarget = document.getElementById('groupSongs')
+  // var spinner = new Spinner(opts).spin(target);
+
+  if (groupTarget != null) {
+    var groupSpinner = new Spinner().spin()
+    groupTarget.appendChild(groupSpinner.el)
+  }
+
   $.ajax({
     url: "/api/v1/group/platform_playlists",
     type: "POST",
