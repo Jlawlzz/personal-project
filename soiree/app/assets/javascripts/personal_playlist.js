2616e4f13d7f1fa927c1b35c6727aef05d89cd98
Jordan Lawler <jordan.lawler@colorado.edu>
2016-04-11 23:06:35 -0600

    added fixes for common bugs in code

--- soiree/app/assets/javascripts/personal_playlist.js
+++ soiree/app/assets/javascripts/personal_playlist.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
   renderAllSongs();
-})
+});
 
 function renderAllSongs(){
 
-  var target = document.getElementById('personalSongs')
-  // var spinner = new Spinner(opts).spin(target);
+  var target = document.getElementById('personalSongs');
 
-  if (target != null) {
-    var spinner = new Spinner().spin()
-    target.appendChild(spinner.el)
+  if (target !== null) {
+    var spinner = new Spinner().spin();
+    target.appendChild(spinner.el);
 
     $.ajax({
       url: '/api/v1/personal/platform_playlists',
       type: 'POST',
       data: {id: document.querySelector('.playlistId').id},
       success: function(response) {
-        console.log("Success!", response.songs)
+        console.log("Success!", response.songs);
       }
-    })
+    });
   }
 
 }
--- soiree/app/helpers/application_helper.rb
+++ soiree/app/helpers/application_helper.rb
@@ -7,4 +7,19 @@ module ApplicationHelper
     [prefix, label, count, max_updated].join("-")
   end
 
+  def bootstrap_class_for flash_type
+    { success: 'alert-success', error: 'alert-danger', warning: 'alert-warning'}[flash_type.to_sym]
+ end
+
+def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "text-center alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat content_tag(:button, 'x'.html_safe, class: 'close', data: {dismiss: 'alert'})
+        concat message
+      end)
+      flash.clear
+    end
+    nil
+  end
+
 end
